f5d0b71061bcd82ab7d855bd9d4fb99a14643249
Souhail <chaouechi.souhail@gmail.com>
2015-12-06 19:56:55 +0100

    TG-10 #closed Intégrer Git/GitHub DVCS et Intellij Idea IDE

---
+++ src/main/java/tn/insat/JeBouquineApplication.java
@@ -0,0 +1,12 @@
+package tn.insat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeBouquineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeBouquineApplication.class, args);
+    }
+}
